b7bd81ae5362f082f54cb17fce20041a7770a4c5
Erik Wiklund <wiklunde@student.chalmers.se>
2012-11-20 21:05:49 +0100

    Minor API tests.

--- src/com/chalmers/frapp/MainActivity.java
+++ src/com/chalmers/frapp/MainActivity.java
@@ -3,13 +3,26 @@ package com.chalmers.frapp;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.*;
 
 public class MainActivity extends Activity {
+
+    private static final String[] CONTENT = new String[] {
+        "HA, HA2", "HA, HA3", "HB, HB2", "HB, HB3", "EDIT, Studion"
+    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, CONTENT);
+
+        AutoCompleteTextView w = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView1);
+        w.setThreshold(1);
+        w.setAdapter(adapter);
+        //finish();
     }
 
     @Override
