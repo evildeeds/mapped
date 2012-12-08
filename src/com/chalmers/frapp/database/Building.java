fc0e1caa8e831285d1edb7247a1cd2a1dbd9adc6
Erik Wiklund <wiklunde@student.chalmers.se>
2012-12-08 16:46:46 +0100

    Added documentation.

--- src/com/chalmers/frapp/database/Building.java
+++ src/com/chalmers/frapp/database/Building.java
@@ -9,10 +9,24 @@ import java.util.TreeMap;
 
 import com.google.android.maps.GeoPoint;
 
+/**
+ * This class represents a single building.
+ */
 public class Building {
 
+	/**
+	 * The name of this building.
+	 */
 	private final String name;
+	/**
+	 * A map containing all entrances, for quickly finding an entrance
+	 * given an entrance ID.
+	 */
 	private final Map<String, Entrance> entrances;
+	/**
+	 * A map containing all rooms in this building, for quickly finding
+	 * a room given its name.
+	 */
 	private final Map<String, Room> rooms;
 
 	public Building(String buildingName) {
@@ -21,32 +35,71 @@ public class Building {
 		rooms = new TreeMap<String, Room>();
 	}
 
+	/**
+	 * Add a new entrance to this building.
+	 * 
+	 * @param entranceID a String representing the entrance ID
+	 * @param streetAddress a String representing the street address
+	 * @param longitude an integer representing the longitude coordinate
+	 * @param latitude an integer representing the latitude coordinate
+	 */
 	public void addEntrence(String entranceID, String streetAddress, int longitude, int latitude) {
 		GeoPoint location = new GeoPoint(longitude, latitude);
 		entrances.put(entranceID, new Entrance(streetAddress, location));
 	}
 
+	/**
+	 * Add a new room to this building.
+	 * 
+	 * @param name a String representing the name of the new room
+	 * @param description a String representing a description of the new room
+	 * @param entranceIDs a List of entranceIDs recommended to reach the new room
+	 * @param longitude an integer representing the longitude coordinate
+	 * @param latitude an integer representing the latitude coordinate
+	 */
 	public void addRoom(String name, String description, List<String> entranceIDs, int longitude, int latitude) {
 		GeoPoint location = new GeoPoint(longitude, latitude);
 		rooms.put(name, new Room(name, description, entranceIDs, location));
 	}
 
+	/**
+	 * Get the name of this building.
+	 * 
+	 * @return a String representing the name of this building
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Get all entrances into this building.
+	 * @return a Collection of entrances into this building
+	 */
 	public Collection<Entrance> getEntrances() {
 		return entrances.values();
 	}
 
+	/**
+	 * Get all rooms in this building.
+	 * @return a Collection of rooms in this building.
+	 */
 	public Collection<Room> getRooms() {
 		return rooms.values();
 	}
-	
+
+	/**
+	 * Find all recommended entrances for a given room.
+	 * 
+	 * @param roomName a String representing the name of a room in this building.
+	 * @return a List of entrances recommended for the given room
+	 * @throws InvalidParameterException if the room is not present in this building
+	 */
 	public List<Entrance> findEntrances(String roomName) throws InvalidParameterException {
 		Room roomObject = rooms.get(roomName);
 		List<Entrance> entranceList = null;
+		// Verify that the given room actually is a part of this building.
 		if(roomObject != null) {
+			// Generate the result list by fetching all entrance IDs and resolving the entrance.
 			entranceList = new LinkedList<Entrance>();
 			for(String entrenceID : roomObject.getEntranceIDs()) {
 				entranceList.add(entrances.get(entrenceID));
--- src/com/chalmers/frapp/database/Room.java
+++ src/com/chalmers/frapp/database/Room.java
@@ -3,11 +3,26 @@ package com.chalmers.frapp.database;
 import java.util.List;
 import com.google.android.maps.GeoPoint;
 
+/**
+ * This class represents information about a single room.
+ */
 public class Room {
 
+	/**
+	 * Room name.
+	 */
 	private final String name;
+	/**
+	 * Room description.
+	 */
 	private final String description;
+	/**
+	 * List of entrance IDs.
+	 */
 	private final List<String> entranceIDs;
+	/**
+	 * Room GPS location.
+	 */
 	private final GeoPoint location;
 	
 	public Room(String name, String description, List<String> entranceIDs, GeoPoint location) {
@@ -17,18 +32,34 @@ public class Room {
 		this.location = location;
 	}
 
+	/**
+	 * Get the name of this room.
+	 * @return a String representing a room name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Get the description for this room.
+	 * @return a String representing a description for this room
+	 */
 	public String getDescription() {
 		return description;
 	}
 
+	/**
+	 * Get the list of entrance IDs for this room.
+	 * @return a List of Strings each representing an ID
+	 */
 	public List<String> getEntranceIDs() {
 		return entranceIDs;
 	}
 
+	/**
+	 * Get the location for this room.
+	 * @return a GeoPoint representing the location of this room
+	 */
 	public GeoPoint getLocation() {
 		return location;
 	}
