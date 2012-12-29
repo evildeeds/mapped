8ed3c05c836a1be350645a38c2b511eef0eae234
Erik Wiklund <wiklunde@student.chalmers.se>
2012-12-29 15:01:27 +0100

    Added testing branch

---
+++ tests/src/com/chalmers/frapp/test/ParseTest.java
@@ -0,0 +1,20 @@
+package com.chalmers.frapp.test;
+
+import android.test.AndroidTestCase;
+import com.chalmers.frapp.database.Parser;
+
+public class ParseTest extends AndroidTestCase {
+
+	public ParseTest(String name) {
+		super();
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	public final void testParser() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
